e561f562748ea6beb3d60d9bc817aa5bb8f26256
BL-Jotiram <jotiram.bandgar@bridgelabz.com>
2021-01-09 14:16:11 +0530

    ADDED: code to chech employee is present or not

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,10 @@
 public class EmpWageBuilder{
 	public static void main(String args[]){
-		System.out.println("Welcome to EmpWageBuilder");	
+		int IS_FULL_TIME = 1;
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		if (empCheck == IS_FULL_TIME)
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Abscent");		
 	}
 }
